0bc7bd9429dee4dff6b43ee774483ad20546df00
theorangepotato <github@alphahotelbravo.io>
2024-07-08 22:48:33 -0700

    Fix EDITOR on krypton
    No idea why neon has the right EDITOR, but this should be a safe change

--- nix/home-manager/common.nix
+++ nix/home-manager/common.nix
@@ -82,7 +82,7 @@
   #
   # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "nvim";
   };
 
   # Let Home Manager install and manage itself.
