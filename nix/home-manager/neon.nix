a042fa8e35568f7cdc8b29654480d1a642156fef
theorangepotato <github@alphahotelbravo.io>
2024-07-19 18:04:49 -0700

    Add autostart programs to nix, instead of configuring manually

--- nix/home-manager/neon.nix
+++ nix/home-manager/neon.nix
@@ -1,4 +1,39 @@
-{pkgs, ...}:
+{pkgs, lib, ...}:
+# https://github.com/nix-community/home-manager/issues/3447#issuecomment-2213029759
+let autostartPrograms = prgms: builtins.listToAttrs (map
+    (pkg: {
+      name = ".config/autostart/" + pkg.pname + ".desktop";
+      value =
+        if pkg ? desktopItem
+        then {
+          # Application has a desktopItem entry.
+          # Assume that it was made with makeDesktopEntry, which exposes a
+          # text attribute with the contents of the .desktop file
+          text = pkg.desktopItem.text;
+        }
+        else {
+          # Application does *not* have a desktopItem entry. Try to find a
+          # matching .desktop name in /share/applications
+          source = with builtins; let
+            appsPath = "${pkg}/share/applications";
+            # function to filter out subdirs of /share/applications
+            filterFiles = dirContents: lib.attrsets.filterAttrs (_: fileType: elem fileType ["regular" "symlink"]) dirContents;
+          in (
+            # if there's a desktop file by the app's pname, use that
+            if (pathExists "${appsPath}/${pkg.pname}.desktop")
+            then "${appsPath}/${pkg.pname}.desktop"
+            # if there's not, find the first desktop file in the app's directory and assume that's good enough
+            else
+              (
+                if pathExists "${appsPath}"
+                then "${appsPath}/${head (attrNames (filterFiles (readDir "${appsPath}")))}"
+                else throw "no desktop file for app ${pkg.pname}"
+              )
+          );
+        };
+    })
+    prgms);
+in
 {
   imports = [
     ./common.nix
@@ -45,6 +80,8 @@
       interval = "30m";
     };
   };
+
+  home.file = autostartPrograms [ pkgs.keepassxc pkgs.signal-desktop pkgs.nextcloud-client pkgs.joplin-desktop pkgs.evolution ];
 
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
